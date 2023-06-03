55a13c1e5a1a6c7ab830fd5cd79724a3cd17207d
Vikas Chauhan <vikas@rockstarchef.in>
2023-06-03 14:26:22 +0530

    initial commit for react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+
 import './App.css';
+import {
+  BrowserRouter, Routes, Route, Redirect
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
