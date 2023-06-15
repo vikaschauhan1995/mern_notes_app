664e7cd8beeb8a47e85445a4c55bd2b75227c9eb
Vikas Chauhan <vikas@rockstarchef.in>
2023-06-15 14:54:25 +0530

    noteDetails card style completed

--- frontend/src/components/NoteDetails.js
+++ frontend/src/components/NoteDetails.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { NOTE_DESCRIPTION, NOTE_TITLE } from '../redux/Note/constants'
 import { useDispatch } from 'react-redux';
 import { deleteNoteFromDBAction } from '../redux/Note/actions';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import '../style/NoteDetails.scss';
 
 const NoteDetails = ({ note }) => {
   const dispatch = useDispatch();
@@ -10,14 +13,26 @@ const NoteDetails = ({ note }) => {
     dispatch(deleteNoteFromDBAction(id));
   }
   return (
-    <div>
-      <h4><b>Title:</b>{note?.[NOTE_TITLE]}</h4>
-      <p><b>Description:</b>{note?.[NOTE_DESCRIPTION]}</p>
-      {/* <!-- suffix for ago --> */}
-      <p>{formatDistanceToNow(new Date(note?.createdAt), { addSuffix: true })}</p>
-      <span onClick={() => deleteNote(note?._id)}>Delete</span>
-    </div>
-  )
+    <Card className="mt-2">
+      <Card.Body>
+        <Card.Title><strong>Title:</strong> {note?.[NOTE_TITLE]}</Card.Title>
+        <Card.Text>
+          <strong>Description:</strong> {note?.[NOTE_DESCRIPTION]}
+        </Card.Text>
+        <samp>{formatDistanceToNow(new Date(note?.createdAt), { addSuffix: true })}</samp>
+        <Button variant="primary" style={{ float: 'right' }} onClick={() => deleteNote(note?._id)}>Delete</Button>
+      </Card.Body>
+    </Card>
+  );
+  // return (
+  //   <div>
+  //     <h4><b>Title:</b>{note?.[NOTE_TITLE]}</h4>
+  //     <p><b>Description:</b>{note?.[NOTE_DESCRIPTION]}</p>
+  //     {/* <!-- suffix for ago --> */}
+  //     <p>{formatDistanceToNow(new Date(note?.createdAt), { addSuffix: true })}</p>
+  //     <span onClick={() => deleteNote(note?._id)}>Delete</span>
+  //   </div>
+  // )
 }
 
 export default NoteDetails;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -25,9 +25,9 @@ const Home = () => {
           <Col xs="12" sm="8" md="8" lg="6">
             <NoteForm />
             {notesState[FETCH_NOTES_ERROR] && <div>{notesState[FETCH_NOTES_ERROR]}</div>}
-            {notesList && notesList.map((note, index) =>
+            {notesList.length > 0 ? notesList.map((note, index) =>
               <NoteDetails key={note?._id + index} note={note} />
-            )}
+            ) : <p>Noting is here</p>}
           </Col>
         </Row>
       </Container>
